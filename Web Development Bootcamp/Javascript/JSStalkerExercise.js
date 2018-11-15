36546c08df3b47a1420f2b9b759327d51cc46098
saiiiyan <kunalaggarwal58@gmail.com>
2018-11-15 19:34:30 +0530

    JSStalkerExercise completed

---
+++ Web Development Bootcamp/Javascript/JSStalkerExercise.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Enter First Name");
+var lastName = prompt("Enter Last Name");
+var age = prompt("Enter Age");
+
+console.log("Your full name is "+firstName+" "+lastName);
+console.log("You are "+age+" years old");
