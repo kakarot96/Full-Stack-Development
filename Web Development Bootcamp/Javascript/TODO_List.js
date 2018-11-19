1694256347ca75ffdc3ac346c65d11218fda4ef8
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2018-11-19 14:25:53 +0530

    TODO_List Part 2 completed

--- Web Development Bootcamp/Javascript/TODO_List.js
+++ Web Development Bootcamp/Javascript/TODO_List.js
@@ -4,13 +4,41 @@ window.setTimeout(function(){
     var input = prompt("What would you like to do?");
 
     while(input !== "quit"){
+        
         if(input === "new"){
-            var newTodo = prompt("Enter a new todo");
-            todos.push(newTodo);
+          addTodo();  
         }
         else if(input === "list"){
-            console.log(todos);
+            listTodo();
+        }
+        else if(input === "delete"){
+
+           delTodo();
         }
         input = prompt("What would you like to do?");
     }
+    console.log("Ok! You have quit the APP");
 },500);
+
+
+
+
+function listTodo(){
+    console.log("**********");
+    todos.forEach(function(todo,index){
+         console.log(index+". "+todo);
+    });
+    console.log("**********");
+}
+
+function delTodo(){
+    var idx = prompt("Enter the index you want to delete");
+    todos.splice(idx,1);
+    console.log("Todo deleted");
+}
+
+function addTodo(){
+    var newTodo = prompt("Enter a new todo");
+    todos.push(newTodo);
+    console.log("Todo added");
+}
