e3d558ecb1f26b21f5ead711403e9cd350cd6477
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2018-11-22 11:16:50 +0530

    Color Guessing Game part 5 completed

--- Web Development Bootcamp/Color Guessing Game/ColorGuess.js
+++ Web Development Bootcamp/Color Guessing Game/ColorGuess.js
@@ -3,35 +3,53 @@ var boxes  = document.querySelectorAll(".box");
 var answer = Math.round(Math.random()*(boxes.length-1));
 var guess = document.getElementById("guess");
 var message = document.getElementById("message");
+var reset = document.getElementById("reset");
+
+reset.addEventListener("click", function(){
+generateAndSetColors(boxes.length);
+message.textContent = "";
+this.textContent = "New Colors";
 
 
+});
+
+generateAndSetColors(boxes.length);
 
 
-function random_rgb() {
-    var o = Math.round, r = Math.random, s = 255;
-    return 'rgb(' + o(r()*s) + ', ' + o(r()*s) + ', ' + o(r()*s)  + ')';
-}
 for(var i=0;i<boxes.length;i++){
-    var color = random_rgb();
-    colors.push(color);
-    boxes[i].style.backgroundColor = color;
+  
 
     boxes[i].addEventListener("click",function(){
         if(this.style.backgroundColor === colors[answer]){
-           console.log("Pass");
            message.textContent = "Correct";
            changeColors(answer);
+           reset.textContent = "Play Again?";
         }
         else{
             this.style.backgroundColor="#212121";
             message.textContent = "Try Again";
-            console.log("Fail");
+            
         }
     });
 }
-guess.textContent = colors[answer];
+
 function changeColors(answer){
     for(var i=0;i<boxes.length;i++){
         boxes[i].style.backgroundColor=colors[answer];
     }
+}
+function random_rgb() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgb(' + o(r()*s) + ', ' + o(r()*s) + ', ' + o(r()*s)  + ')';
+}
+
+function generateAndSetColors(num){
+    colors = [];
+    for(var i=0;i<num;i++){
+        var color = random_rgb();
+        colors.push(color);
+        boxes[i].style.backgroundColor = color;
+    }
+    answer = Math.round(Math.random()*(num-1));
+    guess.textContent = colors[answer];
 }
