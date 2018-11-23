7a940ddb71a030d2d038bccbd91796eddf2ce55c
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2018-11-23 15:04:03 +0530

    Todo Project default list updated

---
+++ assets/js/todo.js
@@ -0,0 +1,26 @@
+// Toggle status of a todo
+$("ul").on("click","li",function(){
+$(this).toggleClass("done");
+});
+
+//Delete a todo from the list
+$("ul").on("click","span",function(event){
+
+$(this).parent().fadeOut(500,function(){
+    $(this).remove();
+});
+event.stopPropagation();
+});
+
+$("input[type=text]").keypress(function(event){
+    if(event.which === 13){
+        var newTodo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> "+newTodo+"</li>");
+        console.log("enter is clicked");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type=text]").fadeToggle();
+});
