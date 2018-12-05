46f549da393264f98fae6d285648eaf7de4e2a27
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2018-12-05 04:06:48 +0000

    RESTful Blog App: Edit and Update

--- Web Development Bootcamp/RESTfullBlogApp/app.js
+++ Web Development Bootcamp/RESTfullBlogApp/app.js
@@ -1,13 +1,16 @@
 var express = require("express"),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override");
 var app = express();
 
 // APP CONFIG
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 mongoose.connect("mongodb://localhost/restful_blog_app");
+
 
 // MONGOOSE CONFIG
 
@@ -54,6 +57,28 @@ app.get("/blogs/:id", function(req, res) {
         }
         else {
             res.render("show", { blog: foundBlog });
+        }
+    })
+});
+app.put("/blogs/:id", function(req, res) {
+    //res.send("updated");
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, foundBlog) {
+        if (err) {
+            console.log("ERROR!!");
+        }
+        else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+app.get("/blogs/:id/edit", function(req, res) {
+
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            console.log("ERROR!!");
+        }
+        else {
+            res.render("edit", { blog: foundBlog });
         }
     })
 });
