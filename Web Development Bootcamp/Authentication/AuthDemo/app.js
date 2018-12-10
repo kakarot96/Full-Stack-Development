b085e9eb368c3966725c0549bd1bcfdddf2f00d6
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2018-12-10 09:50:07 +0000

    Secret Page Code Along 2

--- Web Development Bootcamp/Authentication/AuthDemo/app.js
+++ Web Development Bootcamp/Authentication/AuthDemo/app.js
@@ -1,13 +1,27 @@
 var express = require("express"),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    localStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user")
 var app = express();
 
 // APP CONFIG
 app.set("view engine", "ejs");
+app.use(require("express-session")({
+    secret: "My username is kakarot96",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
 mongoose.connect("mongodb://localhost/auth_demo_app");
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("home");
